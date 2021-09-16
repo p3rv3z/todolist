775c6172c49a96456b5692c4a5aad814120ad3ff
pervezali98 <contact.pervezali@gmail.com>
2021-09-16 14:40:26 +0600

    task crud completed for api

---
+++ server/app/Controllers/Http/TasksController.ts
@@ -0,0 +1,42 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+
+import Task from 'App/Models/Task'
+
+export default class TasksController {
+  public async index() {
+    return Task.query().orderBy('id', 'desc')
+  }
+
+  public async store({ request, response }: HttpContextContract) {
+    const taskSchema = schema.create({
+      title: schema.string({}, [rules.maxLength(250)]),
+    })
+
+    const payload = await request.validate({ schema: taskSchema })
+    const task = await Task.create(payload)
+
+    response.status(201)
+    return task.refresh()
+  }
+
+  public async show({ params }: HttpContextContract) {
+    return Task.findOrFail(params.id)
+  }
+
+  public async update({ params, request }: HttpContextContract) {
+    const taskSchema = schema.create({
+      title: schema.string({}, [rules.maxLength(100)]),
+    })
+
+    const payload = await request.validate({ schema: taskSchema })
+    
+    return (await Task.findOrFail(params.id)).merge(payload).save()
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const task = await Task.findOrFail(params.id)
+    await task.delete()
+    return task
+  }
+}
---
+++ server/database/migrations/1631384007322_tasks.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Tasks extends BaseSchema {
+  protected tableName = 'tasks'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title', 255)
+      table.boolean('is_completed')
+        .defaultTo(false)
+        .comment('Here, 1 means completed and 0 means not completed yet.')
+        
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
