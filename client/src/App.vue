8ce7121751516aaa957c0e20f78dcb31a61736d1
pervezali98 <contact.pervezali@gmail.com>
2021-09-16 17:43:19 +0600

    initial vue setup

---
+++ client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-app-bar app>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>Todolist</v-toolbar-title>
+      </v-app-bar>
+
+      <v-main>
+        <router-view />
+      </v-main>
+    </v-app>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ client/src/main.js
@@ -0,0 +1,18 @@
+import 'vuetify/dist/vuetify.min.css'
+import Vuetify from 'vuetify'
+
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false
+
+Vue.use(Vuetify);
+
+new Vue({
+  vuetify : new Vuetify(),
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
